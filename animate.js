b444a79c97ba40a8edf15e34200f826382c8a8ff
soe43 <soe433@gmail.com>
2018-02-08 15:07:21 -0500

    new work

--- animate.js
+++ animate.js
@@ -2,13 +2,16 @@ var canvas = document.getElementById("board");
 var ctx = canvas.getContext("2d");
 var clearButt = document.getElementById("clear");
 var stopButt = document.getElementById("stop");
+var circButt = document.getElementById("circle");
+var dvdButt = document.getElementById("dvd");
 
 var requestID;
 
-var animate = function(){
+var animateCirc = function(){
     var radius = 0;
     var isSmall = true;
     ctx.fillStyle = "pink";
+    stop();
     var circ=function(){
 	clear();
 	ctx.beginPath();
@@ -35,6 +38,32 @@ var animate = function(){
     circ();
 }
 
+var animateDVD = function(){
+    var previousX;
+    var previousY;
+    var currentX = 250;
+    var currentY = 250;
+    var angle = Math.PI / 3;
+    
+    stop();
+    clear();
+    ctx.arc(currentX,currentY,25,0,2*Math.PI);
+    ctx.stroke();
+    
+    var move = function(){
+	previousX = currentX;
+	previousY = currentY;
+	currentX += Math.sin(angle);
+	currentY += Math.cos(angle);
+	ctx.arc(currentX,currentY,25,0,2*Math.PI);
+	ctx.stroke();
+	window.requestAnimationFrame(move);
+    }
+
+    move();
+    
+}
+
 var clear = function(){
     ctx.clearRect(0,0,500,500);
 }
@@ -43,7 +72,8 @@ var stop = function(){
     window.cancelAnimationFrame(requestID);
 }
 
-canvas.addEventListener("click", animate);
+circButt.addEventListener("click", animateCirc);
 clearButt.addEventListener("click", clear);
 stopButt.addEventListener("click", stop);
+dvdButt.addEventListeer("click" animateDVD);
     
